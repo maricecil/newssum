1e76e394dc1c4bc85a43d045308f2da26b7e33e2
maricecil <ekdnjs035@gmail.com>
2025-01-22 20:24:11 +0900

    aws9

--- newsdocs/settings.py
+++ newsdocs/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '3.36.66.47']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '3.34.130.172']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
